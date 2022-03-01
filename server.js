7fc0ba29f4d45bd7c7cf5a988cb8f2a6fd3b8361
angymai94 <an_gy94@yahoo.it>
2022-03-01 17:35:05 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express= require('express');
+var app= express();
+app.set('port', process.env.PORT|| 3000);
+app.get('/', function(req,res){
+    res.send('Ciao Mondo, sono una APP DI PROVA')
+});
+app.listen(app.get('port'), function(){
+console.log('Esempio di web app sulla porta '+app.get('port'));
+});
