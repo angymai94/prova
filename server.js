84cd53519c1ce92fa548237fa1f9a0dc977f4a77
angymai94 <an_gy94@yahoo.it>
2022-03-02 09:47:52 +0100

    first deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express= require('express');
 var app= express();
 app.set('port', process.env.PORT|| 3000);
 app.get('/', function(req,res){
-    res.send('Ciao Mondo, sono una APP DI PROVA')
+    res.send('Ciao Mondo, sono una APP DI PROVA-first deploy')
 });
 app.listen(app.get('port'), function(){
 console.log('Esempio di web app sulla porta '+app.get('port'));
